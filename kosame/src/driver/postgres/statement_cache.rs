23a3c391d5d0ed1ca418327b6742a125f38d6163
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-12-14 13:53:05 +0100

    Start cached clients

--- kosame/src/driver/postgres/statement_cache.rs
+++ kosame/src/driver/postgres/statement_cache.rs
@@ -1,4 +1,5 @@
-use postgres::{Client, Error, Statement};
+use ::postgres_types::ToSql;
+use postgres::{Client, Error, RowIter, Statement, Transaction};
 
 use crate::driver::postgres_types;
 
@@ -25,3 +26,80 @@ impl postgres_types::StatementCacheClient for Client {
 /// This is a convenience type that specializes [`postgres_types::StatementCache`] for use with
 /// [`postgres::Client`]. It provides all the same functionality as the generic cache.
 pub type StatementCache = postgres_types::StatementCache<postgres::Client>;
+
+pub struct CachedClient {
+    inner: Client,
+    statement_cache: StatementCache,
+}
+
+impl CachedClient {
+    #[must_use]
+    pub fn new(inner: Client) -> Self {
+        Self {
+            inner,
+            statement_cache: StatementCache::new(),
+        }
+    }
+
+    pub async fn query_cached<T, P, I>(
+        &mut self,
+        statement: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<RowIter, Error> {
+        let statement = self
+            .statement_cache
+            .prepare(&mut self.inner, statement.into())
+            .await?;
+        self.inner.query_raw(&statement, params.iter().map(|v| *v))
+    }
+
+    pub async fn transaction(&mut self) -> Result<CachedTransaction<'_>, Error> {
+        Ok(CachedTransaction {
+            inner: self.inner.transaction()?,
+            statement_cache: &mut self.statement_cache,
+        })
+    }
+
+    pub fn inner(&self) -> &Client {
+        &self.inner
+    }
+
+    pub fn statement_cache(&self) -> &StatementCache {
+        &self.statement_cache
+    }
+
+    pub fn statement_cache_mut(&mut self) -> &mut StatementCache {
+        &mut self.statement_cache
+    }
+}
+
+pub struct CachedTransaction<'a> {
+    inner: Transaction<'a>,
+    statement_cache: &'a mut StatementCache,
+}
+
+impl CachedTransaction<'_> {
+    pub async fn query_cached<T, P, I>(
+        &mut self,
+        statement: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<RowIter, Error> {
+        let statement = self
+            .statement_cache
+            .prepare(&mut self.inner.client(), statement.into())
+            .await?;
+        self.inner.query_raw(&statement, params.iter().map(|v| *v))
+    }
+
+    pub fn inner(&self) -> &Transaction {
+        &self.inner
+    }
+
+    pub fn statement_cache(&self) -> &StatementCache {
+        &self.statement_cache
+    }
+
+    pub fn statement_cache_mut(&mut self) -> &mut StatementCache {
+        &mut self.statement_cache
+    }
+}
--- kosame/src/driver/tokio_postgres/statement_cache.rs
+++ kosame/src/driver/tokio_postgres/statement_cache.rs
@@ -1,13 +1,12 @@
-use tokio_postgres::{Client, Error, Statement};
+use postgres_types::ToSql;
+use tokio_postgres::{Client, Error, RowStream, Statement, Transaction};
 
-use crate::driver::postgres_types;
-
-/// Implementation of [`postgres_types::StatementCacheClient`] for asynchronous PostgreSQL client.
+/// Implementation of [`StatementCacheClient`] for asynchronous PostgreSQL client.
 ///
 /// This implementation allows [`tokio_postgres::Client`] to be used with the generic statement cache.
 /// Unlike the synchronous `postgres` implementation, this one is truly asynchronous and uses `.await`
 /// when preparing statements.
-impl postgres_types::StatementCacheClient for Client {
+impl crate::driver::postgres_types::StatementCacheClient for Client {
     type Statement = Statement;
     type Error = Error;
 
@@ -22,6 +21,87 @@ impl postgres_types::StatementCacheClient for Client {
 
 /// Type alias for a statement cache using the asynchronous PostgreSQL client.
 ///
-/// This is a convenience type that specializes [`postgres_types::StatementCache`] for use with
+/// This is a convenience type that specializes [`StatementCache`] for use with
 /// [`tokio_postgres::Client`]. It provides all the same functionality as the generic cache.
-pub type StatementCache = postgres_types::StatementCache<tokio_postgres::Client>;
+pub type StatementCache = crate::driver::postgres_types::StatementCache<tokio_postgres::Client>;
+
+pub struct CachedClient {
+    inner: Client,
+    statement_cache: StatementCache,
+}
+
+impl CachedClient {
+    #[must_use]
+    pub fn new(inner: Client) -> Self {
+        Self {
+            inner,
+            statement_cache: StatementCache::new(),
+        }
+    }
+
+    pub async fn query_cached<T, P, I>(
+        &mut self,
+        statement: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<RowStream, Error> {
+        let statement = self
+            .statement_cache
+            .prepare(&mut self.inner, statement.into())
+            .await?;
+        self.inner
+            .query_raw(&statement, params.iter().map(|v| *v))
+            .await
+    }
+
+    pub async fn transaction(&mut self) -> Result<CachedTransaction<'_>, Error> {
+        Ok(CachedTransaction {
+            inner: self.inner.transaction().await?,
+            statement_cache: &mut self.statement_cache,
+        })
+    }
+
+    pub fn inner(&self) -> &Client {
+        &self.inner
+    }
+
+    pub fn statement_cache(&self) -> &StatementCache {
+        &self.statement_cache
+    }
+
+    pub fn statement_cache_mut(&mut self) -> &mut StatementCache {
+        &mut self.statement_cache
+    }
+}
+
+pub struct CachedTransaction<'a> {
+    inner: Transaction<'a>,
+    statement_cache: &'a mut StatementCache,
+}
+
+impl CachedTransaction<'_> {
+    pub async fn query_cached<T, P, I>(
+        &mut self,
+        statement: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<RowStream, Error> {
+        let statement = self
+            .statement_cache
+            .prepare(&mut self.inner.client(), statement.into())
+            .await?;
+        self.inner
+            .query_raw(&statement, params.iter().map(|v| *v))
+            .await
+    }
+
+    pub fn inner(&self) -> &Transaction {
+        &self.inner
+    }
+
+    pub fn statement_cache(&self) -> &StatementCache {
+        &self.statement_cache
+    }
+
+    pub fn statement_cache_mut(&mut self) -> &mut StatementCache {
+        &mut self.statement_cache
+    }
+}
